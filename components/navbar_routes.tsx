ca3a480f3f7d91df5e1169b518c7d82c4c919049
leeeyoo <leeeyoo@gmail.com>
2023-12-29 12:26:24 +0900

    fix: navbar_routes style

--- components/navbar_routes.tsx
+++ components/navbar_routes.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs"
 
 export const NavbarRoutes = () => {
   return (
-    <div className="flex gap-2 ml-auto">
+    <div className="flex gap-x-2 ml-auto">
       <UserButton />
     </div>
   )
